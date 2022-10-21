97fc6e1c32c358a9d4a886fc50e08815b6351b28
JGL <me@joelgethinlewis.com>
2022-10-21 15:35:13 +0100

    Cleaning up guify

--- docs/js/guify/src/components/partials/label.js
+++
@@ -1,9 +0,0 @@
-import "./label.css";
-
-// eslint-disable-next-line no-unused-vars
-export default (root, text, theme) => {
-    var label = root.appendChild(document.createElement("div"));
-    label.classList.add("guify-component-label");
-    label.innerHTML = text;
-    return label;
-};
--- docs/js/guify/src/guify.js
+++
@@ -1,5 +0,0 @@
-// Export GUI class as 'guify'.
-// Make sure Webpack is assigning the export to the global scope (don't set library.name)
-import { default as gui } from "./gui.js";
-let guify = gui;
-export { guify };
